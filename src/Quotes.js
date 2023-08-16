3171b8890d8b0e7f04ca68977b75f7985fa28e53
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-08-16 17:35:41 -0600

    Added initial content and styling

---
+++ src/Quotes.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Quotes() {
+  return (
+    <div className="Quotes">
+      <h2>"Quote"</h2>
+      <h3 className="mb-5">-Author</h3>
+      <button className="btn btn-primary">New quote</button>
+    </div>
+  );
+}
