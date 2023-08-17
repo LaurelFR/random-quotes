7e68f6c872c9d4f0e4b97582464c2ebd3baa42b5
LaurelFR <128193405+LaurelFR@users.noreply.github.com>
2023-08-16 20:15:42 -0600

    Added new styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
-import Quotes from "./Quotes";
 
 function App() {
   return (
     <div className="App">
-      <div className="container">
-        <Quotes />
+      <div className="quote-box">
+        <h2 className="quote-text">
+          "I attribute my success to this: I never gave or took any excuse."
+        </h2>
+        <h4 className="author">-Florence Nightingale</h4>
+        <div className="buttons">
+          <button className="btn new-quote-button">New quote</button>
+        </div>
       </div>
     </div>
   );
